7fd1de6fa88b7116b34d8d588957570fe798e6bb
John Beieler <john.b30@gmail.com>
2018-06-23 19:08:09 -0400

    Allow PDF directory structure to use scraper name.

--- Scraping_Project/Template_Scrapers/scraperutils.py
+++ Scraping_Project/Template_Scrapers/scraperutils.py
@@ -6,17 +6,17 @@ from collections import OrderedDict
 from datetime import datetime
 
 
-def _get_data(path, project_id):
+def _get_data(path, scraper_name, project_id):
     """Private function to get the absolute path to the downloaded file."""
     cwd = os.path.abspath(os.path.dirname(__file__))
-    return os.path.join(cwd, 'pdf', project_id, path)
+    return os.path.join(cwd, 'pdf', scraper_name, project_id, path)
 
 
-def check_make_directory(project_id):
+def check_make_directory(scraper_name, project_id):
     """Check to see if the `pdf/$PROJECT_ID` dir exists. If not, make it."""
     cwd = os.path.abspath(os.path.dirname(__file__))
-    if not os.path.exists(os.path.join(cwd, 'pdf', project_id)):
-        os.makedirs(os.path.join(cwd, 'pdf', project_id))
+    if not os.path.exists(os.path.join(cwd, 'pdf', scraper_name, project_id)):
+        os.makedirs(os.path.join(cwd, 'pdf', scraper_name, project_id))
 
 
 def get_complete_project_row(project_details):
@@ -52,7 +52,7 @@ def write_csv(scraper_name, data):
                               encoding='utf-8', index=False)  
 
 
-def download_project_documents(url, project_id):
+def download_project_documents(url, scraper_name, project_id):
     """
     Function to download PDF documents given a URL. Currently creates a `pdf`
     directory in PWD and then subdirs for each project ID.
@@ -60,7 +60,7 @@ def download_project_documents(url, project_id):
     Copy-pasta from https://stackoverflow.com/a/16696317.
     """
     filename = url.split('/')[-1]
-    local_filename = _get_data(filename, project_id)
+    local_filename = _get_data(filename, scraper_name, project_id)
     print('Downloading {}'.format(url))
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
